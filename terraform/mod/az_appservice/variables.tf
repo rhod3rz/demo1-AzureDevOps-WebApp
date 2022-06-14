32f438f46afd28d026254793aa3712c83d749dde
rhod3rz <rhodri.freer@outlook.com>
2022-06-14 19:05:55 +0100

    initial upload

---
+++ terraform/mod/az_appservice/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {}
+variable "resource_group_name" {}
+variable "log_analytics_workspace_name" {}
+variable "appi_name" {}
+variable "plan_name" {}
+variable "webapp_name" {}
+variable "acr_login_server" {}
+variable "acr_admin_username" {}
+variable "connection_string" {}
+variable "autoscale_name" {}
